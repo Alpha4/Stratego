99e7cbc9fdb14b618b41b7643eb42a33ac906a16
rogor <rogor@live.fr>
2013-12-27 22:25:57 +0100

    debut de fonction d'affichage du plateau, encore beaucoup de choses a modifier

--- interface.h
+++ interface.h
@@ -8,7 +8,7 @@ Contient également les constantes nécessaires
 #ifndef _INTERFACE_H
     #define _INTERFACE_H
 
-    #define TAILLE_CASE         34 // Taille d'une case du plateau
+    #define TAILLE_CASE         64 // Taille en pixels d'une case du plateau
     #define NB_BLOCS_COTE       10 // Nombre de cases par coté du plateau
     #define LARGEUR_FENETRE     TAILLE_CASE * NB_BLOCS_COTE
     #define HAUTEUR_FENETRE     TAILLE_CASE * NB_BLOCS_COTE
