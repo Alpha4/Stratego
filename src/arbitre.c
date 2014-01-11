0e38028ac5c6b495e755c5db2266a779f6aa4e02
chris44700 <christophe.hubert93@gmail.com>
2014-01-11 16:38:13 +0100

    Update arbitre.c
    il reste à gérer les allés retours

--- src/arbitre.c
+++ src/arbitre.c
@@ -19,5 +19,37 @@ Fonctions utilisées pour l'arbitrage du jeu
 
 int isValidMove(SGameState *gameState, SMove move, EColor currentPlayer)
 {
+    // Coordonnées case initiale
+    int i0 = move.start.line;
+    int j0 = move.start.col;
+    // Coordonnées case finale
+    int i1 = move.end.line;
+    int j1 = move.end.col;
+    // Nombre de cases de déplacement en horizontal et en vertical
+    int movei = abs(i1 -i0);
+    int movej = abs(j1-j0);
+
+    // Lignes et colonnes hors du plateau
+    if ((i0 < 0) || (i0 > 9) || (i1 < 0) || (i1 > 9)) return 0;
+    if ((j0 < 0) || (j0 > 9) || (j1 < 0) || (j1 > 9)) return 0;
+
+    // La pièce sélectionnée n'est pas celle du joueur courant
+    if (gameState->board[i0][j0].content != currentPlayer) return 0;
+    // La pièce sélectionnée est une bombe ou le drapeau (pièce fixe)
+    if ((gameState->board[i0][j0].piece == 0) || (gameState->board[i0][j0].piece == 11)) return 0;
+
+    // La case d'arrivé est un lac ou est occupée par une pièce du joueur courant
+    if ((gameState->board[i0][j0].conent == currentPlayer) || (gameState->board[i0][j0].conent == EClake)) return 0;
+
+    if (movei == movej) return 0; // Déplacement diagonal ou surplace
+    if (gameState->board[i0][j0].piece != 2) // La pièce n'est pas un éclaireur
+    {
+        if ((movei != 0) && (movej != 1)) return 0; // Déplacement vertical >= 2
+        if ((movei != 1) && ((movej != 0)) return 0; // Déplacement horizontal >= 2
+
+     }
+    else if ((movei != 0) && (movej != 0)) return 0; // La pièce est un éclaireur
+
+
     return 1;
 }
